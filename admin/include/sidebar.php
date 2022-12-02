4846354455f97d534f36dba1fdb3d755fc7c87e8
Ebit Gunawan <ebitgunawan2@gmail.com>
2022-12-02 18:59:53 +0700

    rename user

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -104,7 +104,7 @@
                 <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $_SESSION['profile']['nama_user'] ?></span>
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= @$_SESSION['profile']['nama_user'] ?></span>
                         <img class="img-profile rounded-circle" src="../vendor/img/undraw_profile.svg">
                     </a>
                     <!-- Dropdown - User Information -->
